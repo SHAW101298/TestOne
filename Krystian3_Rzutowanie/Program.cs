59ea61792e8d078862316c5f7ff7cb2e73929698
SHAW101298 <SHAW101298@gmail.com>
2021-08-25 21:25:27 +0200

    Test_Commit

---
+++ Krystian3_Rzutowanie/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Krystian3_Rzutowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass nowy_obiekt = new BaseClass();
+            DerivedClass drugi_obiekt = (DerivedClass)nowy_obiekt;
+            NextDerivedClass trzeci_obiekt = (NextDerivedClass)drugi_obiekt;
+
+
+        }
+    }
+}
